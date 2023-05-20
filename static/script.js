b6a07ca0c29976aa031caf3ce3a32ff658b3416d
Jeffrey Wang <j3ff123y@gmail.com>
2023-05-20 17:11:17 -0500

    local bounding box

--- static/script.js
+++ static/script.js
@@ -1,31 +1,71 @@
 const video = document.getElementById('video')
+const canvas = document.getElementById('videoCanvas');
+var ctx = canvas.getContext('2d');
+
+
+function startVideo() {
+  navigator.mediaDevices.getUserMedia({ 
+    video: {
+      mandatory: {
+        maxHeight: 240,
+        maxWidth: 427
+      }
+    }, audio: false 
+  })
+  .then(stream => {
+    video.srcObject = stream;
+  })
+  .catch( err => console.error(err))
+}
 
 Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri('/static/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/static/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/static/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/static/models')
-]).then(startVideo)+
+]).then(startVideo)
 
-function startVideo() {
-  navigator.mediaDevices.getUserMedia(
-    { video: {} },
-    stream => video.srcObject = stream,
-    err => console.error(err)
-  )
+async function drawVideo(results){ //every 150 ms
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+
+  canvas.width = width;
+  canvas.height = height;
+
+  ctx.drawImage(video,0,0, width,height);
+  results.forEach(result =>{
+    var bbox = result._box
+    var rectX = bbox._x;
+    var rectY = bbox._y; 
+    var rectWidth = bbox._width;
+    var rectHeight = bbox._height;
+    ctx.beginPath();
+    ctx.lineWidth = '2';
+    ctx.strokeStyle = 'red';
+    ctx.rect(rectX, rectY, rectWidth, rectHeight);
+    ctx.stroke();
+
+    // ctx.font = "bold 18px serif";
+    // ctx.fillStyle = "#ff0000";
+    // ctx.fillText(
+    //   bbox.p-2, 
+    //   rectX+Math.floor(rectWidth*0.2),
+    //   rectY+Math.floor(rectHeight*0.2)
+    // );
+  })
 }
 
 video.addEventListener('play', () => {
-  const canvas = faceapi.createCanvasFromMedia(video)
-  document.body.append(canvas)
+  const canvas2 = faceapi.createCanvasFromMedia(video)
+  document.body.append(canvas2)
   const displaySize = { width: video.width, height: video.height }
-  faceapi.matchDimensions(canvas, displaySize)
+  faceapi.matchDimensions(canvas2, displaySize)
   setInterval(async () => {
-    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
-    const resizedDetections = faceapi.resizeResults(detections, displaySize)
-    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-    faceapi.draw.drawDetections(canvas, resizedDetections)
-    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-  }, 100)
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+    // const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    // canvas2.getContext('2d').clearRect(0, 0, canvas2.width, canvas2.height)
+    if(detections[0]!=null){
+      drawVideo(detections);
+      console.log(detections)
+      // console.log(tstBox._x,tstBox._y,tstBox._height,tstBox._width)
+    }
+  }, 1000)
 })
+
